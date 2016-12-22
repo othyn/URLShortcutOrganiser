c224eac1e8135216c7af91490e230129d9b8afea
othyn <benny369@hotmail.co.uk>
2016-12-22 15:27:11 +0000

    JSON out working and file removal, next JSON in

--- script.py
+++ script.py
@@ -19,17 +19,45 @@ import json
 # pprint for similar behavior of PHP's var_dump, pprint.pprint(VAR), helps with debugging
 # Spread across multiple lines as per C0410
 
+DIR_TO_SEARCH = ''
+# For storing the directory argument in
+
+REMOVE_FILES_ONCE_PROCESSED = False
+# For storing whether the user would like to remove the files once processed
+
+OLD_URLS_JSON_DIR = ''
+# For storing the location of the old JSON data
+
+OLD_URLS = {}
+NEW_URLS = {}
+# Used for storing old (read) and new (to be written) urls [JSON]
+
 PARSER = argparse.ArgumentParser(description='A directory is required to scan for *.url files.')
 # Creates a new argparse module, setting the description for the scripts arguments
 
 PARSER.add_argument('-d', '--directory', help='Directory to search', required=True)
-# Add an argument with the required information
+# For a directory to run the script on
+
+PARSER.add_argument('-r', '--remove', help='Delete the files as they are processed',
+                    required=False, action='store_true')
+# If the user would like to remove the file once it is processed
+
+PARSER.add_argument('-j', '--json',
+                    help='Instead of creating a new output file, add to an existing JSON data set',
+                    required=False)
+# Ask for a directory to run the script on
 
 ARGS = PARSER.parse_args()
 # Get the argument values
 
 DIR_TO_SEARCH = ARGS.directory
 # Set the directory to search
+
+REMOVE_FILES_ONCE_PROCESSED = ARGS.remove
+# Set the users preference on file deletion
+
+OLD_URLS_JSON_DIR = ARGS.json
+# Set the value of the JSON dir
 
 #pprint.pprint(DIR_TO_SEARCH)
 
@@ -46,10 +74,7 @@ os.chdir(DIR_TO_SEARCH)
 
 for current_file in glob.glob("*.url"):
 
-    #current_file = unicode(current_file, 'utf-8', errors='ignore')
-
     if not os.path.isfile(current_file):
-        print 'ALERT: contains unicode, just fucking skip it for now'
         continue
     # At this moment, no easy solution for dealing with filenames containing unicode
     # So, run a check to see if the file exists, if not, its probably unicode
@@ -57,8 +82,18 @@ for current_file in glob.glob("*.url"):
     with open(current_file, "r") as infile:
         for line in infile:
             if line.startswith('URL'):
-                print line[4:]
+                NEW_URLS[current_file] = line[4:]
                 break
+    # Get the URL from the current file and add it into the dict
+
+    if REMOVE_FILES_ONCE_PROCESSED:
+        #os.remove(current_file)
+        print 'remove'
 # Iterate through each file that ends in url
 # Check the file exists
-# If it does, grab the url
+# If it does, add the filename and url to the dict
+# Remove the file if the user has specified to do so
+
+with open('url_data.json', 'w') as file_output:
+    json.dump(NEW_URLS, file_output, sort_keys=True, indent=4, ensure_ascii=False)
+# Create a new JSON file in the current directory and dump the processed dict
